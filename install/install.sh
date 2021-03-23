4f05df138e6b15891a52a3137f9313f6e3e256c4
Erik Schröder <malte-erik.schroeder@airbus.com>
2021-03-23 11:45:42 +0100

    Add install script

---
+++ install/install.sh
@@ -0,0 +1,15 @@
+# install packer
+git clone https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# install language server
+sudo npm install -g graphql-language-service-cli \
+	vscode-css-languageserver-bin \
+	vim-language-server \
+	bash-language-server \
+	dockerfile-language-server-nodejs \
+	typescript typescript-language-server \
+	yaml-language-server \
+	vscode-html-languageserver-bin \
+	pyright \
+	vscode-json-languageserver
